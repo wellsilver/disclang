822af2e5f6b859c1755f4cdeb99816add8753d52
wellsilver <wellsilver2016@gmail.com>
2022-05-19 12:18:31 -0400

    add libraries I made

---
+++ libaries/chat.py
@@ -0,0 +1,6 @@
+def main(i):
+  a=i.split('[',1)[1]
+  b=a.split('[',1)[1]
+  a=a.split(']',1)[0]
+  b=b.split(']',1)[0]
+  return f"If c == '{a}':\n  await l.channel.send('{b}')"
---
+++ libaries/out.py
@@ -0,0 +1,5 @@
+def main(i):
+  a=i.split('[',1)[1]
+  a=a.split(']',1)[0]
+  return f"print('{a}')"
+  
