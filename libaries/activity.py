5fb24da5a61870aa93000945d08d6e8b8f3c97a5
wellsilver <wellsilver2016@gmail.com>
2022-05-19 12:15:24 -0400

    Create activity.py

---
+++ libaries/activity.py
@@ -0,0 +1,6 @@
+def main(i):
+  a=i.split('[',1)[1]
+  b=a.split('[',1)[1]
+  a=a.split(']',1)[0]
+  b=b.split(']',1)[0]
+  return f'await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="{b}")'
