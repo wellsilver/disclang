baa90e39c643053efd11d674f355b748c65087de
wellsilver <wellsilver2016@gmail.com>
2023-11-04 17:36:35 -0400

    base

--- lib/out.py
+++
@@ -1,4 +0,0 @@
-import lib.libi
-def main(i):
-  a=lib.libi.getargs(i,1)
-  return f"  print('{a[0]}')"
--- lib/reply.py
+++
@@ -1,7 +0,0 @@
-def main(i):
-  a=i.split('[',1)[1]
-  b=a.split('[',1)[1]
-  a=a.split(']',1)[0]
-  b=b.split(']',1)[0]
-  return f"  if c == '{a}':\n    await l.reply('{b}')"
-  
